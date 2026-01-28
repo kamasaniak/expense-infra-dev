d007cee7215e47b969c50b3e4e9747aac5a92be7
rajendra <akrajendra597@.com>
2026-01-28 08:58:15 +0530

    terraform-aws-eks

--- terraform-aws-eks/20-bastion/bastion.tf
+++ terraform-aws-eks/20-bastion/bastion.tf
@@ -1,8 +1,16 @@
-resource "aws_instance" "bastion" {
-  ami                    = data.aws_ami.joindevops.id
+resource "aws_instance" "this" {
+  ami                    = "ami-09c813fb71547fc4f" # This is our devops-practice AMI ID
   vpc_security_group_ids = [data.aws_ssm_parameter.bastion_sg_id.value]
   instance_type          = "t3.micro"
   subnet_id   = local.public_subnet_id
+
+  # 20GB is not enough
+  root_block_device {
+    volume_size = 50  # Set root volume size to 50GB
+    volume_type = "gp3"  # Use gp3 for better performance (optional)
+  }
+  user_data = file("bastion.sh")
+  
   tags = merge(
     var.common_tags,
     {
